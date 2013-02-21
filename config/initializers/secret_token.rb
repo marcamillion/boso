2df0085321c335da4d7a14f763217804f0e716e1
Marc Gayle <marc.gayle@gmail.com>
2013-02-21 16:07:28 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boso::Application.config.secret_token = 'f36811b604ce37e144054a9bd140c7dbdb238fa51f87e2ac5fc7ceeade9ebd504093a527f9fb76736580e9be01d4d12be38047ad7d86c16c31a7c7d44c7bac7d'
