42b09fc869bfcc9771365daad26b4e5bc165208f
Marc Gayle <marc.gayle@gmail.com>
2013-03-28 11:56:31 -0500

    Got some basic highlighting working. But still haven't figured out pygments issue with highlighting within a <code> tag within a <pre> tag.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,24 @@
 module ApplicationHelper
+    class HTMLwithPygments < Redcarpet::Render::HTML
+      def block_code(code, language)
+        Pygments.highlight(code, lexer:language)
+      end
+    end
+    
+    def markdown(text)
+        renderer = HTMLwithPygments.new(hard_wrap: true)
+        options = {
+          autolink: true,
+          no_intra_emphasis: true,
+          fenced_code_blocks: true,
+          lax_html_blocks: true,
+          strikethrough: true,
+          superscript: true
+        }
+        Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+    end
+  
+  
 
   def display_base_errors resource
     return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
